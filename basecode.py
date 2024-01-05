2ef37bd6803b96d820c7cc7da5cc1e38c9de0dac
PVM <70097274+Phammanh26@users.noreply.github.com>
2024-01-05 20:53:17 +0700

    fix: resolve conflict

--- basecode.py
+++ basecode.py
@@ -1,2 +1,5 @@
 print("Hello base code")
-print("With update code need edit base")
+
+#print("With update code need edit base")
+#print("change in delete feature")
+print("resolve:fix conflict cherry-pick")
