d8def998be0ecce5df9aa6b91cb39ae6a4b33583
PVM <70097274+Phammanh26@users.noreply.github.com>
2024-01-07 14:29:38 +0700

    add new logic for sonalint

--- sonalint-refactor.py
+++ sonalint-refactor.py
@@ -4,3 +4,9 @@ def sonalint():
 
 def sonalint_v2():
 	print("hello sonalint 2")
+
+# thêm mới 1 số thay đổi trong khi những thay đổi trước đó 
+# của nhánh này đang được taoh 1 PR 
+#từ sonalint refactor branch -> develop
+def add_new_logic():
+	print("abc")
