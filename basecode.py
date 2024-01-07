43c85b57b4130f3b8c1ec9819ac7d2fa6c7f9542
PVM <70097274+Phammanh26@users.noreply.github.com>
2024-01-07 14:19:36 +0700

    add new logic base

--- basecode.py
+++ basecode.py
@@ -6,6 +6,10 @@ def explain():
 def fix_hello_world():
 	print("hello world")
 
+
+def base_sonalint_explain():
+	print("day la function phai dc day len develop ngay để các tính năng khác được sử dụng")
+
 def base_sonalint():
 	print("sonalint")
 
