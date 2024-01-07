26ca6e4ef9a4620858c537aa7f347f8dd96dbdcd
PVM <70097274+Phammanh26@users.noreply.github.com>
2024-01-07 13:50:37 +0700

    update new changes from develop

--- basecode.py
+++ basecode.py
@@ -1,3 +1,11 @@
-print("Hello base code")
+
 def explain():
 	print("explain")
+
+# đồng thuận sử dụng phương án này cùng với develop branch
+def fix_hello_world():
+	print("hello world")
+
+def base_sonalint():
+	print("sonalint")
+
