bdc0c836f9a94c6afd3a0fff335c2ef2d308e289
PVM <70097274+Phammanh26@users.noreply.github.com>
2024-01-08 09:36:31 +0700

    hotfix: fix missing meg resolve conflict from previous PR

--- basecode.py
+++ basecode.py
@@ -1,12 +1,8 @@
 print("Hello base code")
 
-<<<<<<< HEAD
-def explain:
-	print("explain")
-=======
+
 def sonarlint():
 	print("hello sonarlint")
 def explain:
 	print("explain")
 
->>>>>>> 5a53cccb17dfdaab92c1cc21e4e931a24f5e896a
